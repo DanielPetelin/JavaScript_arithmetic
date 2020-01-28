fecb6fb59beda4616517c304b4f860ed5074118b
Daniil Petelin <DaniilPetelin77@gmail.com>
2020-01-28 19:04:53 +0300

    add final commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
+if (document.getElementById('num1').value == '') {
+    document.getElementById('num1').value = '0';
+};
+
+if (document.getElementById('num2').value == '') {
+    document.getElementById('num2').value = '0';
+};
+
 function plus() {
+    if (document.getElementById('num1').value == '') {
+        document.getElementById('num1').value = '0';
+    };
+    
+    if (document.getElementById('num2').value == '') {
+        document.getElementById('num2').value = '0';
+    };
+
     var num1, num2, result;
         num1 = document.getElementById('num1').value;
         num1 = parseInt(num1);
@@ -6,12 +22,20 @@ function plus() {
         num2 = document.getElementById('num2').value;
         num2 = parseInt(num2);
 
-        result = num1 + num2;
+        result = num1 + num2 || num2 + num1;
 
     document.getElementById('out').innerHTML = result;
 };
 
 function minus() {
+    if (document.getElementById('num1').value == '') {
+        document.getElementById('num1').value = '0';
+    };
+    
+    if (document.getElementById('num2').value == '') {
+        document.getElementById('num2').value = '0';
+    };
+
     var num1, num2, result;
         num1 = document.getElementById('num1').value;
         num1 = parseInt(num1);
@@ -25,6 +49,14 @@ function minus() {
 };
 
 function composition() {
+    if (document.getElementById('num1').value == '') {
+        document.getElementById('num1').value = '0';
+    };
+    
+    if (document.getElementById('num2').value == '') {
+        document.getElementById('num2').value = '0';
+    };
+
     var num1, num2, result;
         num1 = document.getElementById('num1').value;
         num1 = parseInt(num1);
@@ -32,12 +64,20 @@ function composition() {
         num2 = document.getElementById('num2').value;
         num2 = parseInt(num2);
 
-        result = num1 * num2;
+        result = num1 * num2 || num2 * num1;
 
     document.getElementById('out').innerHTML = result;
 };
 
 function division() {
+    if (document.getElementById('num1').value == '') {
+        document.getElementById('num1').value = '0';
+    };
+    
+    if (document.getElementById('num2').value == '') {
+        document.getElementById('num2').value = '0';
+    };
+
     var num1, num2, result;
         num1 = document.getElementById('num1').value;
         num1 = parseInt(num1);
@@ -46,19 +86,60 @@ function division() {
         num2 = parseInt(num2);
 
         result = num1 / num2;
+        
+    if (document.getElementById('num1').value == 0 && document.getElementById('num1').value == 0) {
+        result = 'error';
+    };   
 
     document.getElementById('out').innerHTML = result;
 };
 
+function divisionNotNum() {
+    if (document.getElementById('num1').value == '') {
+        document.getElementById('num1').value = '0';
+    };
+    
+    if (document.getElementById('num2').value == '') {
+        document.getElementById('num2').value = '0';
+    };
+
+    var num1, num2, result;
+        num1 = document.getElementById('num1').value;
+        num1 = parseInt(num1);
+
+        num2 = document.getElementById('num2').value;
+        num2 = parseInt(num2);
+
+        result = num1 / num2;
+        result.toFixed(1);
+
+    if (document.getElementById('num1').value == 0 && document.getElementById('num1').value == 0) {
+        result = 'error';
+    };    
+
+    document.getElementById('out').innerHTML = result.toFixed(1);
+};
+
 function degree() {
-var num1 = document.getElementById('num1').value;
-    num1 = parseInt(num1);
+    if (document.getElementById('num1').value == '') {
+        document.getElementById('num1').value = '0';
+    };
+    
+    if (document.getElementById('num2').value == '') {
+        document.getElementById('num2').value = '0';
+    };
 
-var num2 = document.getElementById('num2').value;
-    num2 = parseInt(num2);
+    var num1 = document.getElementById('num1').value;
+        num1 = parseInt(num1);
 
-    result = Math.pow(num1, num2);
+    var num2 = document.getElementById('num2').value;
+        num2 = parseInt(num2);
+
+        result = Math.pow(num1, num2);
+
+    if (document.getElementById('num1').value == 0) {
+        result = 0;
+    };    
 
     document.getElementById('out').innerHTML = result;
-
 };
